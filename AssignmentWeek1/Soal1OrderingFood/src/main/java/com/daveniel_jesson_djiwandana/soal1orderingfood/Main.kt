21476aef945beb1dd3ff938bf7f5ec3b005a85ee
Daveniel Jesson Djiwandana <ddjiwandana@student.ciputra.ac.id>
2026-09-16 13:40:45 +0700

    part 1 Soal 1 Main Menu

--- AssignmentWeek1/Soal1OrderingFood/src/main/java/com/daveniel_jesson_djiwandana/soal1orderingfood/Main.kt
+++ AssignmentWeek1/Soal1OrderingFood/src/main/java/com/daveniel_jesson_djiwandana/soal1orderingfood/Main.kt
@@ -1,5 +1,31 @@
 package com.daveniel_jesson_djiwandana.soal1orderingfood
 
 fun main() {
-    println("Hello World")
+    var condition: Boolean = true;
+    while (condition) {
+        println("""ORDER SYSTEM
+1. Make Order
+2. View Orders
+3. View Menu
+4. Add Menu
+5. Edit Menu
+6. Delete Menu
+7. Exit""".trimMargin())
+        print("Pick A Number: ")
+        val choice = readln()
+        if (choice == "7") {
+            println("byebye")
+            return
+        }
+        if (choice == "1" || choice == "2" || choice == "3" || choice == "4" || choice == "5" || choice == "6") {
+            println(choice)
+            mainMenuOptions(choice.toInt())
+        } else {
+            println("wrong input\n")
+        }
+    }
+
+}
+fun mainMenuOptions (input: Int) {
+
 }
